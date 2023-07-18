e40c3f765faad9c9a27043948a22e29423b80b36
Chris McNamara <cmcnamara1991@icloud.com>
2023-07-18 11:13:08 -0500

    Pull the position array from type Shift and add it to User in typeDefs.js

--- server/models/Address.js
+++ server/models/Address.js
@@ -1,30 +1,30 @@
 const { Schema } = require("mongoose");
 
 const addressSchema = new Schema({
-  street1: {
-    type: String,
-    required: true,
-  },
-  street2: {
-    type: String,
-    required: true,
-  },
-  city: {
-    type: String,
-    required: true,
-  },
-  state: {
-    type: String,
-    required: true,
-  },
-  zip: {
-    type: String,
-    required: true,
-  },
-  country: {
-    type: String,
-    required: true,
-  },
+    street1: {
+        type: String,
+        required: true,
+    },
+    street2: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    state: {
+        type: String,
+        required: true,
+    },
+    zip: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
 });
 
 module.exports = addressSchema;
