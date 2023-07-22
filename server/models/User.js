7e6a2218c13b7520afc16e2444e0a5b6dedb0c1e
Chris McNamara <cmcnamara1991@icloud.com>
2023-07-21 21:38:04 -0500

    Remove unwanted code inUser.js model and delete controllers and routes folder.

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,5 @@
 const { Schema, model } = require('mongoose');
 // const bcrypt = require('bcrypt');
-// const { v4: uuidv4 } = require('uuid');
 // const shiftSchema = require('./Shift');
 // const positionSchema = require('./Position');
 // const companySchema = require('./Company');
@@ -8,11 +7,6 @@ const addressSchema = require("./Address");
 // const accountSchema = require("./Account");
 
 const userSchema = new Schema({
-  // _id: {
-  //   type: String,
-  //   required: true,
-  //   unique: true,
-  // },
   firstName: {
     type: String,
     unique: false,
@@ -23,16 +17,6 @@ const userSchema = new Schema({
     unique: false,
     required: true,
   },
-//   email: {
-//     type: String,
-//     unique: false,
-//     match: [/.+@.+\..+/, "Must match an email address!"],
-//     required: true,
-//   },
-//   password: {
-//     type: String,
-//     required: true,
-//   },
   phone: {
     type: String,
     unique: false,
@@ -91,26 +75,6 @@ const userSchema = new Schema({
     ref: "Account",
   },
 });
-
-
-// userSchema.pre('save', function (next) {
-//   if (this.isNew) {
-//     this._id = uuidv4(); // Set a unique ID for new users
-//   }
-//   next();
-// });
-
-// userSchema.pre('save', async function (next) {
-//     if (this.isNew || this.isModified('password')) {
-//         const saltRounds = 10;
-//         this.password = await bcrypt.hash(this.password, saltRounds);
-//     }
-//     next();
-// })
-// // custom method to compare and validate password for logging in
-// userSchema.methods.isCorrectPassword = async function (password) {
-//         return bcrypt.compare(password, this.password);
-// };
 
 const User = model('User', userSchema);
 
