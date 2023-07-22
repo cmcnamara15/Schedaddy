9cc10b3bc809415b49b316b1ff1f9c38a1d853ea
Hunter H <hunter.hester15@gmail.com>
2023-07-22 09:19:53 -0500

    adds modal encapsulated in anchor tag

--- client/src/components/forms/registerform.js
+++ client/src/components/forms/registerform.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Modal } from 'react-bootstrap';
 
 const RegisterForm = () => {
+
+  const [showModal, setShowModal] = useState(false);
+
+  const handleModalOpen = () => {
+    setShowModal(true)
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false)
+  };
+
+
   return (
-    <div>RegisterForm</div>
+    <>
+    <a onClick={handleModalOpen}>
+      Register here
+    </a>
+    <Modal show={showModal} onHide={handleModalClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Title</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>This is where I could go to register</p>
+      </Modal.Body>
+    </Modal>
+    </>
   )
 }
 
