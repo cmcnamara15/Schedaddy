ebaabc6eebab93ce0a60cf28fef556d20cb914ff
eeast <eeasthope@gmail.com>
2023-07-24 13:56:03 -0500

    adds logged in functions to Positions page and Employees page. Adds landing page for '/' path welcoming user and requesting sign in

--- client/src/components/EmployeeList.js
+++ client/src/components/EmployeeList.js
@@ -4,7 +4,9 @@ import Employee from './partials/Employee';
 import { useQuery } from '@apollo/client';
 import { FIND_ALL_USERS } from '../utils/queries';
 import { FaFileCirclePlus } from 'react-icons/fa6';
+import Auth from '../utils/auth';
 import EmptyUser from './partials/EmptyUser';
+import RequestSignIn from './RequestSignIn';
 
 
 
@@ -25,31 +27,37 @@ const EmployeeList = () => {
   if (error) return <pre>{`Error: ${error.message}`}</pre>
 
   return (
-    <div className='container'>
-      <div className="card">
-        <div className='card-header mb-0'>
-          <div className="row">
-            <div className="col-6">
-              <h1>Employees</h1>
+    <>
+      {Auth.loggedIn() ? (
+        <div className='container'>
+          <div className="card">
+            <div className='card-header mb-0'>
+              <div className="row">
+                <div className="col-6">
+                  <h1>Employees</h1>
+                </div>
+                <div className="col-6 text-end">
+                  <EmployeeForm u={EmptyUser} id='new' button={
+                    <button type='button' className='btn btn-primary' data-bs-toggle='tooltip' data-bs-placement='left' title='Add an Employee'>
+                      <span data-bs-toggle='modal' data-bs-target='#addEmployeeForm-new'>
+                        <FaFileCirclePlus/>
+                      </span>
+                    </button>
+                  }/>
+                </div>
+              </div>
             </div>
-            <div className="col-6 text-end">
-              <EmployeeForm u={EmptyUser} id='new' button={
-                <button type='button' className='btn btn-primary' data-bs-toggle='tooltip' data-bs-placement='left' title='Add an Employee'>
-                  <span data-bs-toggle='modal' data-bs-target='#addEmployeeForm-new'>
-                    <FaFileCirclePlus/>
-                  </span>
-                </button>
-              }/>
+            <div className="card-body">
+              <ul className="list-group list-group-flush">
+                {employees.map((emp) => <Employee u={emp}/>)}
+              </ul>
             </div>
           </div>
         </div>
-        <div className="card-body">
-          <ul className="list-group list-group-flush">
-            {employees.map((emp) => <Employee u={emp}/>)}
-          </ul>
-        </div>
-      </div>
-    </div>
+      ) : (
+        <RequestSignIn/>
+      )}
+    </>
   )
 }
 
